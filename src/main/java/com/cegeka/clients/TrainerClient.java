b41179f6fabc67c1759ec7597d8b80dcd364b100
sachareyskens <11400982@student.pxl.be>
2018-10-10 16:01:53 +0200

    Please jacoco

--- src/main/java/com/cegeka/clients/TrainerClient.java
+++ src/main/java/com/cegeka/clients/TrainerClient.java
@@ -6,24 +6,25 @@ import org.datavec.api.split.FileSplit;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
 
 @Component
 public class TrainerClient {
 
-//    public void trainModel() {
-//        int seed = 123;
-//        double learningRate = 0.01;
-//        int batchSize = 50;
-//        int nEpochs = 30;
-//        int numInputs=25;
-//        int numOutputs=25;
-//        int numHiddenNodes = 20;
-//
-//        // load the training data
-//        RecordReader rr = new CSVRecordReader();
-//        rr.initialize(new FileSplit(new File("d"));
-//
-//
-//
-//    }
+    public void trainModel() throws IOException, InterruptedException {
+        int seed = 123;
+        double learningRate = 0.01;
+        int batchSize = 50;
+        int nEpochs = 30;
+        int numInputs=25;
+        int numOutputs=25;
+        int numHiddenNodes = 20;
+
+        // load the training data
+        RecordReader rr = new CSVRecordReader();
+        rr.initialize(new FileSplit(new File("d")));
+
+
+
+    }
 }
